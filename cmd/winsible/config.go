6799156c166f0ec60e10db010a70eac4cbc220b1
Bergmeier, Andreas <Andreas.Bergmeier@otto.de>
2020-09-18 16:51:31 +0200

    Fix YAML indent
    Autoformatter broke internal YAML config.

--- cmd/winsible/config.go
+++ cmd/winsible/config.go
@@ -9,22 +9,22 @@ import (
 const (
 	taskConfig = `
 - name: Download PowerDesigner
-	gc_storage:
-	bucket: bar
-	object: PowerDesigner165SP04x64.exe
-	dest:   'C:\temp\PowerDesigner165SP04x64.exe'
-	mode:   get
+  gc_storage:
+    bucket: bar
+    object: PowerDesigner165SP04x64.exe
+    dest:   'C:\temp\PowerDesigner165SP04x64.exe'
+    mode:   get
 - name: Download pd.iss
-	gc_storage:
-	bucket: foo
-	object: pd.iss
-	dest:   'C:\temp\pd.iss'
-	mode:   get
+  gc_storage:
+    bucket: foo
+    object: pd.iss
+    dest:   'C:\temp\pd.iss'
+    mode:   get
 - name: Install PowerDesigner
-	win_package:
-	path:       'C:\temp\PowerDesigner165SP04x64.exe'
-	product_id: '{D174290F-9A4E-48E3-9EB5-1B6A8AB67E9B}'
-	arguments:  /s /f1'C:\temp\pd.iss'
+  win_package:
+    path:       'C:\temp\PowerDesigner165SP04x64.exe'
+    product_id: '{D174290F-9A4E-48E3-9EB5-1B6A8AB67E9B}'
+    arguments:  /s /f1'C:\temp\pd.iss'
 `
 )
 
